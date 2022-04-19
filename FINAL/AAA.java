b2a449a02be6b2cd6407c716a596d55d8a4c24eb
epearrr <etahonp@gmail.com>
2022-04-19 14:47:00 -0400

    added Move class

--- FINAL/AAA.java
+++ FINAL/AAA.java
@@ -3,21 +3,26 @@ package FINAL;
 // tester
 public class AAA {
     public static void main(String[] args) {
-        CellStatus cs = CellStatus.SUB_HIT;
-        System.out.println(cs);
+        // CellStatus cs = CellStatus.SUB_HIT;
+        // System.out.println(cs);
 
-        AircraftCarrier a = new AircraftCarrier();
-        Battleship b = new Battleship();
-        Cruiser c = new Cruiser();
-        Destroyer d = new Destroyer();
-        Sub s = new Sub();
+        // AircraftCarrier a = new AircraftCarrier();
+        // Battleship b = new Battleship();
+        // Cruiser c = new Cruiser();
+        // Destroyer d = new Destroyer();
+        // Sub s = new Sub();
 
-        Fleet f = new Fleet(a,b,c,d,s);
+        // Fleet f = new Fleet(a,b,c,d,s);
 
-        System.out.println(f.updateFleet(ShipType.ST_AIRCRAFT_CARRIER));
-        System.out.println(f.updateFleet(ShipType.ST_AIRCRAFT_CARRIER));
-        System.out.println(f.updateFleet(ShipType.ST_AIRCRAFT_CARRIER));
-        System.out.println(f.updateFleet(ShipType.ST_AIRCRAFT_CARRIER));
-        System.out.println(f.updateFleet(ShipType.ST_AIRCRAFT_CARRIER));
+        // System.out.println(f.updateFleet(ShipType.ST_AIRCRAFT_CARRIER));
+        // System.out.println(f.updateFleet(ShipType.ST_AIRCRAFT_CARRIER));
+        // System.out.println(f.updateFleet(ShipType.ST_AIRCRAFT_CARRIER));
+        // System.out.println(f.updateFleet(ShipType.ST_AIRCRAFT_CARRIER));
+        // System.out.println(f.updateFleet(ShipType.ST_AIRCRAFT_CARRIER));
+
+        Move m = new Move(2,6);
+        System.out.println(m.col());
+        System.out.println(m.row());
+        System.out.println(m);
     }
 }
---
+++ FINAL/Move.java
@@ -0,0 +1,49 @@
+package FINAL;
+
+public class Move {
+    private int col;
+    private int row;
+
+    /**
+     * constructor that creates a new move when given two ints as parameters
+     * @param col
+     * @param row
+     */
+    public Move(int col, int row){
+        this.col = col;
+        this.row = row;
+    }
+
+    /**
+     * constructor that creates a new move when given a string such as E2 as a parameter
+     * @param s
+     */
+    public Move(String s){
+        col = (int) s.charAt(0) - 65;
+        row = Integer.parseInt(s.substring(1)) - 1;
+    }
+
+    /**
+     * @return col
+     */
+    public int col(){
+        return col;
+    }
+
+    /**
+     * @return row
+     */
+    public int row(){
+        return row;
+    }
+
+    /**
+     * @return a string represented where the move was consisting of a letter in the range A-J followed by a number in the range 1-10
+     */
+    @Override
+    public String toString(){
+        // use ASCII values to convert col from an int to a char
+        char c = (char) (col + 65);
+        return String.format("%c%d", c, row+1);
+    }
+}
